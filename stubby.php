00ac95622095bedef7e69c9e57a0446f072eff87
Jenn Doe <muhrrynn@users.noreply.github.com>
2017-01-12 22:57:02 -0500

    Create stubby.php

---
+++ stubby.php
@@ -0,0 +1,51 @@
+<?php /* Created on: 11/01/2008 */ ?>
+<div id="footie" class="centered">
+<img src="images/b1.gif" width="50" height="33" alt="" />&nbsp;&nbsp; 
+<a href="mailto:
+ &lt;script type='text/javascript'>
+ &lt;!--
+ var prefix = 'ma' + 'il' + 'to';
+ var path = 'hr' + 'ef' + '=';
+ var addy24417 = 'sa' + 'les' + '@';
+ addy24417 = addy24417 + '---' + '---' + '---' + '.' + 'com';
+ document.write( '&lt;a ' + path + '\'' + prefix + ':' + addy24417 + '\'>' );
+ document.write( addy24417 );
+ document.write( '&lt;\/a>' );
+ //-->\n &lt;/script>&lt;script type='text/javascript'>
+ &lt;!--
+ document.write( '&lt;span style=\'display: none;\'>' );
+ //-->
+ &lt;/script>This e-mail address is being protected from spam bots, JavaScript must be enabled to view it
+ &lt;script type='text/javascript'>
+ &lt;!--
+ document.write( '&lt;/' );
+ document.write( 'span>' );
+ //-->
+ &lt;/script>" title="email to:">
+
+ <script type='text/javascript'>
+ <!--
+ var prefix = 'ma' + 'il' + 'to';
+ var path = 'hr' + 'ef' + '=';
+ var addy24417 = 'sa' + 'les' + '@';
+ addy24417 = addy24417 + '---' + '---' + '---' + '.' + 'com';
+ document.write( '<a ' + path + '\'' + prefix + ':' + addy24417 + '\'>' );
+ document.write( addy24417 );
+ document.write( '<\/a>' );
+ //-->\n </script><script type='text/javascript'>
+ <!--
+ document.write( '<span style=\'display: none;\'>' );
+ //-->
+ </script>This e-mail address is being protected from spam bots, JavaScript must be enabled to view it
+ <script type='text/javascript'>
+ <!--
+ document.write( '</' );
+ document.write( 'span>' );
+ //-->
+ </script></a>&nbsp;&nbsp; 
+<img src="images/b2.gif" width="50" height="33" alt="" />  
+</div>
+
+</div>
+</body>
+</html>
